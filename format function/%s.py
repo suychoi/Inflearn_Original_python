f56b30e38dd766c8bd5ac0fca8fb67fdbba59c17
suychoi <suychoi@gmarket.com>
2022-04-17 17:14:58 +0900

    print format

---
+++ format function/%s.py
@@ -0,0 +1,38 @@
+# VSC 출력은 Ctrl + Shift + B
+print('''이게뭔데''')
+print('python', 'google.com', sep='@')
+
+# End option(개행을 하지 않고 이어쓰기)
+print('Welcome to', end=' ')
+print('IT News', end=' ')
+print('Web Site')
+
+# file option
+import sys
+print('Learn Python', file=sys.stdout) 
+# 외부 파일에 출력내용을 저장한다. 
+
+#Format (d, s, f) 정수, 문자열, 실수
+print('%s %s' % ('one', 'two'))
+print('{} {}'.format('one', '2'))
+# 인덱스 번호에 맞춰서 대입한다.
+print('{1} {0}'.format('one', 'two'))
+
+# %s 사용법 (10칸을 확보해두고  채운다.)
+print('%10s' % ('nice'))    #("      nice")
+print('{:>10}'.format('nice'))  #("      nice")
+
+print('%-10s' % ('nice'))       #("nice     ")
+print('{:10}'.format('nice'))   #("nice     ")
+
+
+print('{:_>10}'.format('nice'))   #("______nice")
+print('{:^10}'.format('nice'))   #("   nice    ")
+
+#문자열의 크기가 더 큰 경우 . 
+# 기호가 있어야 잘린 상태로 출력된다. 
+print('%.5s' % ('pythonstudy')) #("pytho")
+#10공간 중에서 5공간만 출력
+print('{:10.5}'.format('pythonstudy'))  #('pytho     ')
+
+
