1ff16a0e824ede7e6f7529d57f1a97cdc4db1d85
CHOI SU YEOL <jack96318@hanmail.net>
2022-05-20 23:51:44 +0900

    re_oop_1

---
+++ study_again/re_oop_1.py
@@ -0,0 +1,29 @@
+# 클래스 개념, OOP와 절차지향 프로그래밍의 차이
+# 클래스와 인스턴스의 차이를 이해
+# 그리고 인스턴스와 객체의 차이는 ?
+
+#클래스 변수는 : 직접 접근이 가능하고, 공유한다.
+#인스턴스 변수는 : 객체마다 별도로 존재한다.
+
+class Dog():
+    sepcies = 'dog' # 클래스 변수
+
+    def __init__(self, name, age):
+        self.name = name        # 인스턴스 변수, self 가 붙음
+        self.age = age
+
+print(Dog)
+a = Dog('jell', 1)
+b = Dog('ceil', 3)
+print('dog1', a.__dict__)
+print('dog2', b.__dict__)
+# print('dog3', a.__li)
+
+print("The dog1 called {} which age {}".format(a.name, a.age))
+
+if a.sepcies == 'dog':
+    print('{} is a {}'.format(a.name, a.age))
+
+# 클래스 변수 직접 접근하기
+print(Dog.sepcies)
+print(a.sepcies)
