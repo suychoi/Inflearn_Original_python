e5c454538273135e86d3414c0691a570181c03d2
CHOI SU YEOL <jack96318@hanmail.net>
2022-06-01 00:11:25 +0900

    class deepen day2

--- 15_class_deepen/about_class_2-1.py
+++ 15_class_deepen/about_class_2-1.py
@@ -4,6 +4,56 @@
 # 클래스 중심은 언어의 문제가 아니다.
 # 만들고자 하는 프로그램의 규모와 앞으로의 지향(목적)에 따라 클래스 기반이냐 절차 기반이냐를 고민해야 한다.
 
+class Car():
+    """
+    잘만들어진 패키지를 보면 코멘트를 달아서 정의를 해준다.
+    Car Class
+    Author : suychoi
+    Date : 2022-05-31
+    """
+
+    def __init__(self, company, details):
+        self._company = company
+        self._details = details
+
+    # 사용자 레벨에서 print 로 정보를 확인할때는 __str__ 메서드를 구현
+    def __str__(self):  #self 를 받으면 인스턴스 메서드 string
+        return 'str : {} - {}'.format(self._company, self._details)
+
+    # 개발자 레벨에서 텍스트 뿐만아니라 객체의 엄격한 정보를 확인할때는 __repr__ 메서드를 구현
+    # __str__ 이 없으면 print(car1) 할 때 __repr__이 호출됩니다.
+    def __repr__(self): #representation 객체를 그대로 자료형에 타입에따른 객체를 그대로 표시
+        return 'repr : {} - {}'.format(self._company, self._details)
+
+# Self 의미 : 인스턴스 메서드에서는 self가 첫번째 인자로 넘어오게 돼있다.
+
+
+car1 = Car('Ferrari', {'car_company' : 'Ferrari', 'car_detail' : {'color': 'White','horsepower': 400, 'price': 8000}})
+car2 = Car('BMW', {'car_company' : 'BMW', 'car_detail' : {'color': 'Black', 'horsepower': 200,'price': 3000}})
+car3 = Car('Audi',{'car_company' : 'Audi', 'car_detail' : {'color': 'Yellow', 'horsepower': 300, 'price': 4500}})
+
+# ID 값 확인 다 다른것을 확인 -> 클래스는 하나지만, 인스턴스는 여러개다.
+print(id(car1))
+print(id(car2))
+print(id(car3))
+
+print(car1._company == car2._company)
+print(car1 is car2)
+
+# dir & __dict__
+print(dir(car1))
+print(dir(car1) == dir(car2))   #True
+
+# 인스턴스의 namespace만 확인 딕셔너리 형태로 모든 값까지 보여준다.
+print()
+print(car1.__dict__)
+print(car2.__dict__)
+
+
+# Doctring 클래스에 정의된 코멘트 확인
+print(Car.__doc__)
+print()
+
 
 
 
--- 15_class_deepen/about_class_2-2.py
+++ 15_class_deepen/about_class_2-2.py
@@ -5,6 +5,38 @@
 # 만들고자 하는 프로그램의 규모와 앞으로의 지향(목적)에 따라 클래스 기반이냐 절차 기반이냐를 고민해야 한다.
 
 
+class Car():
+    """
+    잘만들어진 패키지를 보면 코멘트를 달아서 정의를 해준다.
+    Car Class
+    Author : suychoi
+    Date : 2022-05-31
+    """
+
+    def __init__(self, company, details):
+        self._company = company
+        self._details = details
+
+    def __str__(self):  #self 를 받으면 인스턴스 메서드 string
+        return 'str : {} - {}'.format(self._company, self._details)
+
+    def __repr__(self): #representation 객체를 그대로 자료형에 타입에따른 객체를 그대로 표시
+        return 'repr : {} - {}'.format(self._company, self._details)
+
+    def detail_info(self):
+        print('Current ID : {}'.format(id(self)))
+        print('Car Detail Info : {} {}'.format(self._company, self._details.get('price')))
+
+
+car1 = Car('Ferrari', {'car_company' : 'Ferrari', 'car_detail' : {'color': 'White','horsepower': 400, 'price': 8000}})
+car2 = Car('BMW', {'car_company' : 'BMW', 'car_detail' : {'color': 'Black', 'horsepower': 200,'price': 3000}})
+car3 = Car('Audi',{'car_company' : 'Audi', 'car_detail' : {'color': 'Yellow', 'horsepower': 300, 'price': 4500}})
+
+# 함수 호출
+car1.detail_info()
+
+# 비교
+print(id(car1.__class__) == id(car2.__class__))
 
 
 
