98ec6edbe709175ea94167416741812f17db9054
CHOI SU YEOL <jack96318@hanmail.net>
2022-06-12 10:39:18 +0900

    sequence day 5

--- 17_sequence/sequence-2.py
+++ 17_sequence/sequence-2.py
@@ -42,10 +42,39 @@ print(m, id(m))
 # sort vs sorted
 # reverse, key=len, key=str.lower, key=func ... key에 내가 만든 기준으로 정렬을 하겠다
 
+# sorted : 정렬 후 새로운 객체 반환(원본 변경 안함)
+# sort : 정렬 후 객체 직접 변경
 
+f_list = ['orange', 'apple', 'strawberry', 'grape', 'peach', 'banana', 'coconut']
+print()
+print('original - ', f_list)
+print('sorted - ', sorted(f_list))
+print('original - ', f_list)
+print('sorted - ', sorted(f_list, reverse=True))  # 알파벳 역순으로 정렬
+print('sorted - ', sorted(f_list, key=len))     # 알파벳 길이순으로 정렬
+print('sorted - ', sorted(f_list, key=lambda x: x[-1]))   # 끝에 문자를 기준으로 정렬
+print('sorted - ', sorted(f_list, key=lambda x: x[-1], reverse=True))
+print('original - ', f_list)
 
+print()
+print()
 
+# sort는 반환값이 (None)
+print('original - ', f_list)
+print('sort - ', f_list.sort(), f_list) # 원본에 수정하는 것
+print('original - ', f_list)
+print('sort - ', f_list.sort(reverse=True), f_list)
+print('original - ', f_list)
+print('sort - ', f_list.sort(key=len), f_list)
+print('original - ', f_list)
+print('sort - ', f_list.sort(key=lambda x: x[-1]), f_list)
+print('original - ', f_list)
+print('sort - ', f_list.sort(key=lambda x: x[-1], reverse=True), f_list)
+print('original - ', f_list)
 
+# List 와 Array의 적합한 사용방법
+# 리스트 기반 : 융통성(다양한data type), 다양한 자료형, 범용적 사용
+# Array 사용 : 숫자 기반, 배열(리스트와 거의 호환)
 
 
 
